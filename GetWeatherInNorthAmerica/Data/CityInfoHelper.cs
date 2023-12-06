2532cbcca5908bc0efb8bbcf6f4a1d49a70c2972
liamastephenson <liamastephenson@gmail.com>
2023-12-06 18:56:21 -0500

    Finished functionality and CSS/HTML Visuals

--- GetWeatherInNorthAmerica/Data/CityInfoHelper.cs
+++ GetWeatherInNorthAmerica/Data/CityInfoHelper.cs
@@ -1,36 +1,59 @@
 ﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Newtonsoft.Json.Linq;
 using RestSharp;
+using static GetWeatherInNorthAmerica.Data.WeatherInfoHelper;
 
 namespace GetWeatherInNorthAmerica.Data
 {
+    public class Cities
+    {
+        public string name {  get; set; }
+        public string country { get; set; }
+        public string state { get; set; }
+        public string lat { get; set; }
+        public string lon { get; set; }
+    }
+
     public class CityInfoHelper
     {
 
-        static string GetCityInfo(string city)
+       public static IList<Cities> GetCityInfo(string city)
         {
             var client = new RestClient("http://api.openweathermap.org");
             var request = new RestRequest
             (
               "http://api.openweathermap.org/geo/1.0/direct"
             );
-
+            Passwords passwords = new Passwords();
             request.AddParameter("q", city);
-            request.AddParameter("limit", "5"); // Set the limit parameter
-            request.AddParameter("appid", apiKey);
+            request.AddParameter("limit", "44"); // Set the limit parameter (44 is the highest recorded number of cities with the same name
+            request.AddParameter("appid", passwords.getApiKey());
 
             var response = client.Execute(request);
 
             if (response.IsSuccessful)
             {
+
+                JArray citiesArray = JArray.Parse(response.Content);
+                IList<Cities> cities = citiesArray.Select(p => new Cities
+                {
+                    name = (string)p["name"],
+                    country = (string)p["country"],
+                    state = (string)p["state"],
+                    lat = (string)p["lat"],
+                    lon = (string)p["lon"]
+                }).ToList();
+
+
                 // Parse and display the weather information
                 Console.WriteLine($"Geo information for {city}:");
                 Console.WriteLine(response.Content);
-                return response.Content;
+                return cities;
             }
             else
             {
                 Console.WriteLine(response.ErrorMessage);
-                return response.ErrorMessage;
+                return null;
             }
 
 
---
+++ GetWeatherInNorthAmerica/Data/WeatherInfoHelper.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using RestSharp;
+using System.Drawing;
+using System;
+using Newtonsoft.Json.Linq;
+
+
+namespace GetWeatherInNorthAmerica.Data
+{
+    public class WeatherReport
+    {
+        public string name { get; set; }   
+        public string state {  get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        
+        public string temp {  get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    
+
+    public class WeatherInfoHelper
+    {
+        
+       public static WeatherReport GetWeatherCity(Cities city)
+        {
+           
+            var client = new RestClient("http://api.openweathermap.org");
+            var request = new RestRequest
+       (
+         "https://api.openweathermap.org/data/2.5/weather"
+       );
+            Passwords passwords = new Passwords();
+            
+
+            request.AddParameter("lat", city.lat);
+            request.AddParameter("lon", city.lon); 
+            request.AddParameter("appid",passwords.getApiKey());
+
+            var response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            var weatherObject = JObject.Parse(response.Content);
+
+            WeatherReport cityWeather = new WeatherReport
+            {
+                name = city.name,
+                main = (string)weatherObject["weather"][0]["main"],
+                state = city.state,
+                description = (string)weatherObject["weather"][0]["description"],
+                temp = (string)weatherObject["main"]["temp"]
+            };
+
+           
+                cityWeather.temp = ""+(double.Parse(cityWeather.temp) - 273.15);
+            
+
+            return cityWeather;
+        }
+    }
+}
